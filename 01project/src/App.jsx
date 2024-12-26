d7f41ddbdb52c7343ca75264e65a86f1edf3e0e9
Rohit Goswami <rohitkumargoswami31@gmail.com>
2024-12-26 17:13:22 +0530

    Day1-Appwrite

--- 01project/src/App.jsx
+++ 01project/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1 className="text-3xl font-bold bg-orange-400 p-4">My New Project</h1>
